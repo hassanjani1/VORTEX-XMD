9171abd33ba8f64b10ee234af7c79950504b2d7d
hassanjani1 <161316228+hassanjani1@users.noreply.github.com>
2025-07-07 18:04:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY 𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY 𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 
@@ -29,13 +29,13 @@ ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/di5kdx.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/6p9zuj.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃",
+BOT_NAME: process.env.BOT_NAME || "𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃",
+STICKER_NAME: process.env.STICKER_NAME || "𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -43,15 +43,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255614195217",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923136197714",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "HansTech",
+OWNER_NAME: process.env.OWNER_NAME || "𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ HansTech*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/di5kdx.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6p9zuj.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> 𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃*⚡ IS ALIVE",
+LIVE_MSG: process.env.LIVE_MSG || "> 𝙍𝙖𝙣𝙖-𝘼𝙬𝙖𝙞𝙨*⚡ IS ALIVE",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -75,7 +75,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255614195217",
+DEV: process.env.DEV || "923136197714",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
